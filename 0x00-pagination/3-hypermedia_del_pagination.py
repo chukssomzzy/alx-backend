5595b388b49d67182a0ed5ad1826dc912f65b6ee
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-30 16:00:23 +0000

    get_hyper_index test on one line

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -39,8 +39,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
-                        page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Get hypermedia by seek pagination
         Args:
             index (int): the starting index
