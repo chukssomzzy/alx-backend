95e8afe216f3f5701db8de18e0419c37ddb2cd6a
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-06 18:39:27 +0000

    properly annotated

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def home():
+def home() -> str:
     """Route to home"""
-    return render_template('0-index.html')
+    return (render_template('0-index.html'))
 
 
 if __name__ == "__main__":
