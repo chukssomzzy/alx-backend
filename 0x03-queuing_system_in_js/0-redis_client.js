ea8702e0c1b79fe91efa4b3bf85913ae47766d03
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-03-05 04:47:17 +0100

    IMPLEMENT: redis connection

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client
+  .on('error', (err) => console.log(`Redis client not connected to the server: ${err}`))
+  .on('connect', () => console.log('Redis client connected to the server'));
