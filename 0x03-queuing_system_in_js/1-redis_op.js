8f08936c8b604906e03cafe04fda0860aaf769d8
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-03-05 05:23:40 +0100

    IMPLEMENT: redis get and set

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,19 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client
+  .on('error', (err) => console.log(`Redis client not connected to the server: ${err}`))
+  .on('connect', () => console.log('Redis client connected to the server'));
+
+function setNewSchool(schoolName, value) {
+  client.SET(schoolName, value, print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.GET(schoolName, (_err, val) => console.log(val));
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
