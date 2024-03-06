884fc6270007dd885e5f4772be9baab410a05d04
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-03-06 18:24:20 +0000

    IMPLEMENTED: job creator

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+import kue from 'kue';
+
+export default function createPushNotificationsJobs(jobs, queue) {
+  if (!jobs instanceof Array) {
+    throw new Error('Jobs is not an array');
+  }
+
+  for (const job in jobs) {
+    const jobCreated = queue.create('push_notification_code_3', job)
+      .save();
+
+    jobCreated
+      .on('enqueue', () => console.log(`Notification job created: ${jobCreated.id}`))
+      .on('complete', () => console.log(`Notification job ${jobCreated.id} completed`))
+      .on('failed', (err) => console.log(`Notification job ${jobCreated.id} failed: ${err}`))
+      .on('progress', (progress) => console.log(`Notification job ${jobCreated.id} ${progress}% complete`))
+  }
+}
